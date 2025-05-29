1bde4bf83a1e71b9652608702189e2af6f34cc15
zhaowei2025 <zhaowei2025@protonmail.com>
2025-05-29 16:02:09 +0800

    fix: add registries configuration to mason-lspconfig to fix nil value error

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -732,12 +732,12 @@ require('lazy').setup({
           "tsserver",
         },
         automatic_installation = true,
+        registries = {
+          "github:mason-org/mason-registry",
+        },
         handlers = {
           function(server_name)
             local server = servers[server_name] or {}
-            -- This handles overriding only values explicitly passed
-            -- by the server configuration above. Useful when disabling
-            -- certain features of an LSP (for example, turning off formatting for ts_ls)
             server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
             require('lspconfig')[server_name].setup(server)
           end
